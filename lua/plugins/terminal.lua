c287d824a546957597d8c155ee0339c479cd9abc
Ron-ron Aspe Rivera <ronaspe42@gmail.com>
2025-12-24 15:01:01 +0800

    added terminal to my setup

---
+++ lua/plugins/terminal.lua
@@ -0,0 +1,18 @@
+return {
+
+
+    {
+        "akinsho/toggleterm.nvim",
+        version = "*",
+        config = function()
+            require("toggleterm").setup({
+                open_mapping = [[<c-\>]],
+                direction = "float",
+                float_opts = {
+                    border = "rounded",
+                },
+            })
+        end,
+    }
+
+}
